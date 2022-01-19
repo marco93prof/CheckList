8abec9f45864812863dc956179c8c90dbab94768
Marco <marco93prof@gmail.com>
2022-01-18 22:32:37 -0300

    UPDATE all file were refactored

--- script.js
+++ script.js
@@ -1,161 +1,122 @@
-function handleControlVars( type, arr ){
-	switch( type ) {
-		
-		case "addTask":
-			lastID += 1;
-			actualTasksArray.push(arr[0]);
-		break;
-		
-		case "eraseTask":
-			//parse task name
-			let taskName = "";
-			for( let i=0; i < arr[0].length - 2; ++i)
-				taskName += arr[0][i];
-			//remove task from array
-			actualTasksArray.splice( actualTasksArray.indexOf(taskName), 1 );
-		break;
-
-		case "eraseList":
-			lastID = 0;
-			actualTasksArray = [];
-		break;
-
-		default:
-			console.log("ERROR | function handleGlobalVars(type, arr)");
-	}
-}
-
-function addTask(element) {
-	let taskName = document.getElementsByClassName("inputTask")[0].value;
-
-	if( taskName == "") {
-		window.alert("Fill the name of the new task.");
-	}	
-
-	// else if( !taskName.match("^[A-Za-z0-9]+$") ) { window.alert("The task name must be alphanumeric."); }
-
-	else if( taskName.length > 64) {
-		window.alert("This task name is too long.");
-	}	
-
-	else if( actualTasksArray.indexOf(taskName) > -1 ){
-		window.alert(`The task "${taskName}" is already on the list.`);
-	}
-
-	else {
-		handleControlVars("addTask", [taskName]);
-		element.target.previousElementSibling.value = "";
-		let tasksList = document.getElementsByClassName("tasksList")[0];
-		tasksList.appendChild( createTaskElement( "item"+lastID, taskName ) );
-	}
-}
-
-function createTaskElement( idName, taskName ){
-	let li = document.createElement("li");
-	li.id = idName;
-	li.className = "taskItem";
-
-	let label = document.createElement("label");
-	label.className = "labelTask";
-	label.innerHTML = `${taskName}<input type="checkbox"><span class="checkmark"></span>`
-	label.addEventListener( "mouseover", destakLabel );
-	label.addEventListener( "mouseout", undestakLabel );
-
-	let divBtn = document.createElement("div");
-	divBtn.className = "btnEraseTask";
-	divBtn.innerHTML = "-";
-	divBtn.addEventListener( "click", eraseTask );
-	divBtn.addEventListener( "mouseover", destakErase );
-	divBtn.addEventListener( "mouseout", undestakErase );
-
-	li.appendChild(label);
-	li.appendChild(divBtn);
+function addItem( element ) {
+	let itemName = document.getElementsByClassName("menu-input")[0].value;
 	
+	if( itemName == "") {
+		alert("Enter an item name.");
+	}
+	else if( itemName.length > 50) {
+		alert("The name of this item is too long.");
+	}
+	else if( currentList.indexOf(itemName) > -1 ) { //GLOBAL VAR
+		alert("This item is already on the list.");
+	}
+	else {
+		element.target.previousElementSibling.value = ""; //clean input text
+		let list = document.getElementsByClassName("list")[0];
+		list.appendChild( newItem( "ID"+nextID, itemName, NOTCHECKED) ); //nextID and NOTCHECKED are GLOBAL
+		//HANDLE GLOBAL CONTROL VARS
+		nextID += 1;
+		currentList.push(itemName);
+		checkedList.push(false);
+	}
+}
+
+function newItem( id, name, checked ){
+	let li = document.createElement("li");
+	li.id = id;
+	li.className = "item";
+
+	let box = document.createElement("div");
+	box.className = "item-box";
+	box.innerHTML = `<div class="item-checkbox ${checked}"><div class="item-checkmark"></div></div><p class="item-text">${name}</p>`;
+	box.addEventListener( "click", clickItem );
+	box.addEventListener( "mouseover", mouseoverItem );
+	box.addEventListener( "mouseout", mouseoutItem );
+
+	let eraseBtn = document.createElement("div");
+	eraseBtn.className = "item-eraseBtn";
+	eraseBtn.innerHTML = "-";
+	eraseBtn.addEventListener( "click", clickEraseBtn );
+	eraseBtn.addEventListener( "mouseover", mouseoverEraseBtn );
+	eraseBtn.addEventListener( "mouseout", mouseoutEraseBtn );
+
+	li.appendChild(box);
+	li.appendChild(eraseBtn);
 	return li;
 }
 
-//=========================================
-function destakLabel( element ) {
-	element.target.style.boxShadow = "0 0 2px #080";
-}
+function clickItem( element ) {
+	let checkbox = element.currentTarget.children[0];
+	if( checkbox.className[14] == "N" ){
+		checkbox.style.backgroundColor = "#090";
+		checkbox.className = `item-checkbox ${YESCHECKED}`;
+		//HANDLE GLOBAL CONTROL VARS
+		let itemName = element.currentTarget.innerText;
+		let pos = currentList.indexOf(itemName);
+		checkedList[pos] = true;
 
-function undestakLabel( element ) {
-	element.target.style.boxShadow = "none";
-}
-
-function destakErase( element ) {
-	element.target.previousElementSibling.style.boxShadow = "0 0 2px #f00";
-}
-
-function undestakErase( element ) {
-	element.target.previousElementSibling.style.boxShadow = "none";
-}
-
-//=========================================
-function eraseTask( element ) {
-	let rmv = element.target.parentElement;
-	handleControlVars("eraseTask", [rmv.innerText]);
-	rmv.remove();
-}
-
-function eraseList(){
-	handleControlVars("eraseList", []);
-	document.getElementsByClassName("tasksList")[0].innerHTML = "";
-	localStorage.clear();
-}
-
-//=========================================
-
-function saveList() {
-	//erase old saved list
-	localStorage.clear();
-
-	//get the raw list (have to parse on "onload" function)
-	let raw = document.getElementsByClassName("tasksList")[0].innerText;
-	
-	//save the actual raw list in ONE key (unique key is to order purpose)
-	localStorage.setItem("rawSavedTasksList", raw);
-}
-
-/////////////////////////////////////////// 
-//                   MAIN 
-/////////////////////////////////////////// 
-
-// GLOBAL CONTROL VARS
-var lastID = 0;
-var actualTasksArray = [];
-
-onload = function() {
-	
-	//Get the list to fill
-	let list = document.getElementsByClassName("tasksList")[0];
-	
-	//Get the Local Storage *RAW* list
-	let raw = localStorage.getItem("rawSavedTasksList");
-	
-	//Parse the raw list
-	for( let i=0; i<raw.length; ) {
-		let taskName = "";
-		while( raw[i] !== "\n" )
-			taskName += raw[i++];
-		i += 3;
-		
-		//Insert the item on list
-		console.log(taskName);
-		handleControlVars("addTask", [taskName]);
-		list.appendChild( createTaskElement( "item"+lastID, taskName ) );
+	}
+	else if( checkbox.className[14] == "Y" ){
+		checkbox.style.backgroundColor = "#fff";
+		checkbox.className = `item-checkbox ${NOTCHECKED}`;		
+		//HANDLE GLOBAL CONTROL VARS
+		let itemName = element.currentTarget.innerText;
+		let pos = currentList.indexOf(itemName);
+		checkedList[pos] = false;
 	}
 }
 
-// Add a new task in the list 
-let btnAdd = document.getElementsByClassName("btnAdd")[0];
-btnAdd.addEventListener( "click", addTask );
+function mouseoverItem( element ) {
+	element.currentTarget.style.boxShadow = "0 0 2px #070";
+}
 
-// Save all tasks of the list
-let btnSaveList = document.getElementsByClassName("btnSaveList")[0];
-btnSaveList.addEventListener( "click", saveList );
+function mouseoutItem( element ) {
+  element.currentTarget.style.boxShadow = "none";
+}
 
-// Erase all tasks of the list
-let btnEraseList = document.getElementsByClassName("btnEraseList")[0];
-btnEraseList.addEventListener( "click", eraseList );
+///////////////////////////////////////////
 
+function eraseList() {
+	document.getElementsByClassName("list")[0].innerHTML = "";
+	//HANDLE GLOBAL CONTROL VARS
+	nextID = 0;
+	currentList = [];
+	checkedList = [];
+}
+
+function clickEraseBtn( element ){
+	//HANDLE GLOBAL CONTROL VARS
+	let itemName = element.target.previousElementSibling.innerText;
+	let position = currentList.indexOf(itemName);
+	currentList.splice( position, 1 );
+	checkedList.splice( position, 1 );
+	//main objective of this function
+	element.target.parentElement.remove();
+}
+
+function mouseoverEraseBtn( element ){
+	element.target.parentElement.children[0].style.boxShadow = "0 0 2px #f00";
+}
+
+function mouseoutEraseBtn( element ){
+	element.target.parentElement.children[0].style.boxShadow = "none";
+}
+
+///////////////////////////////////////////
+// MAIN 
+/////////////////////////////////////////// 
+
+// GLOBAL CONTROL VARS
+var nextID = 0;
+var currentList = [];
+var checkedList = [];
+const NOTCHECKED = "Nchecked"; 
+const YESCHECKED = "Ychecked";
+
+// Add a new item in the list 
+let menuAddBtn = document.getElementsByClassName("menu-addBtn")[0];
+menuAddBtn.addEventListener( "click", addItem );
+
+// Erase all itens of the list
+let menuEraseBtn = document.getElementsByClassName("menu-eraseBtn")[0];
+menuEraseBtn.addEventListener( "click", eraseList );
